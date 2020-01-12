f31439a19088f36753553893ebfdc9c89a32cda3
ezgatrabajo <ezgatrabajo@gmail.com>
2020-01-12 12:34:05 -0300

    results en pagination

--- DRFTutorial/settings.py
+++ DRFTutorial/settings.py
@@ -112,12 +112,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        #'oauth2_provider.ext.rest_framework.OAuth2Authentication',
-        #'rest_framework_social_oauth2.authentication.SocialAuthentication',
+
     ],
     'DEFAULT_PERMISSION_CLASSES':[
       'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
 }
 
 AUTHENTICATION_BACKENDS = {
